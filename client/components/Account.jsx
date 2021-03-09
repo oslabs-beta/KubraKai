60af1a6c246264d6eb589d89114c3f5bf8076104
Jordan <jordanking7@gmail.com>
2021-03-09 11:39:12 -0700

    cpuUsage componenet update

--- client/components/Account.jsx
+++ client/components/Account.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
+import CpuUsage from './cpuUsage.jsx'
+import MetricsContainer from './metricsContainer.jsx'
 
 export default function Account(props) {
     return(
-        <h2>Welcome, you have made it to your account</h2>
+        // <h2>Welcome, you have made it to your account</h2>
+        <div>
+            <CpuUsage />
+            <MetricsContainer />
+        </div>
     )
 }
