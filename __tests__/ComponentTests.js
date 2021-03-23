81d2c201f90d48043517766692ddd8a477ff6d42
Taylor Davis <moosky@live.com>
2021-03-23 10:51:44 -0700

    ready to merge main

--- __tests__/ComponentTests.js
+++ __tests__/ComponentTests.js
@@ -1,7 +1,19 @@
-import subject from '../client/components/ipInput'
+import React from 'react';
+import { configure, mount, render, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+import "regenerator-runtime/runtime.js";
+import { context } from '../client/context';
+import Button from '@material-ui/core/Button'; 
+
+
+import IpInput from '../client/components/ipInput';
+configure({adapter: new Adapter()});
 
 describe('React unit tests', () => {
     describe('ipInput', () => {
+        let wrapper; 
+
         beforeAll(() => {
             let data = {
                 ipArray: [],
@@ -10,10 +22,12 @@ describe('React unit tests', () => {
                    return data
                 }
             }
+            wrapper = mount(<context.Provider value={data}><IpInput /></context.Provider>)
         })
     
         it('Renders a <Button> with the label in Aria', () => {
-            expect(subject()).toEqual(true);
+            console.log(toJson(wrapper))
+            expect(wrapper.contains(<IpInput/>)).toEqual(true);
         }) 
     });
 });
